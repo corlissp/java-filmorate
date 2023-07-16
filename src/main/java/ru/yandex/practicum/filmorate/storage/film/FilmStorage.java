529a9b25942c5a6b714cca64c32d9bfae5be2b31
danilmin <kol15fol@bk.ru>
2023-07-16 23:30:20 +0300

    fix: style

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -8,8 +8,12 @@ import java.util.List;
  * @author Min Danil 13.07.2023
  */
 public interface FilmStorage {
+
     Film addFilmStorage(Film film);
+
     List<Film> getAllFilmsStorage();
+
     Film updateFilmStorage(Film film);
+
     Film getFilmByIdStorage(int id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -8,8 +8,12 @@ import java.util.List;
  * @author Min Danil 13.07.2023
  */
 public interface UserStorage {
+
     User createUserStorage(User user);
+
     User updateUserStorage(User user);
+
     List<User> getAllUsersStorage();
+
     User getUserByIdStorage(int id);
 }
