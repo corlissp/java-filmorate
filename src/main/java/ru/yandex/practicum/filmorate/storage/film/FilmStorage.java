b1acda28810b55f8db82c853d51baa0ad06e9d11
danilmin <kol15fol@bk.ru>
2023-07-16 23:48:27 +0300

    fix: style

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -8,7 +8,6 @@ import java.util.List;
  * @author Min Danil 13.07.2023
  */
 public interface FilmStorage {
-
     Film addFilmStorage(Film film);
 
     List<Film> getAllFilmsStorage();
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.NotFoundForUpdateException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.models.User;
 
 import java.util.ArrayList;
