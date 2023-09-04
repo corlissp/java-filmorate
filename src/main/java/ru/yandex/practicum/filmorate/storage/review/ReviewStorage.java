dfa8652d7cfb0f3dd79627748602e3220db44392
Светлана <kulechko@mail.ru>
2023-09-04 12:34:05 +0300

    add-reviews

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewStorage.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.filmorate.storage.review;
+
+import ru.yandex.practicum.filmorate.models.Review;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ReviewStorage {
+
+    List<Review> getAllReviews();
+
+    Optional<Review> create(Review review);
+
+    Optional<Review> findReviewById(int id);
+
+    Optional<Review> addLike(Integer reviewId, Integer userId);
+
+    Optional<Review> addDislike(Integer reviewId, Integer userId);
+
+    Optional<Review> removeLike(Integer reviewId, Integer userId);
+
+    Optional<Review> update(Review review);
+
+    Optional<Review> delete(int reviewId);
+
+    List<Review> getReviewsByFilmId(int filmId, int count);
+
+
+}
