ca7df1f6591748ef3b040c19e112dbcaefe0676e
Светлана <kulechko@mail.ru>
2023-09-04 14:24:44 +0300

    111

--- src/main/java/ru/yandex/practicum/filmorate/controllers/ReviewController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/ReviewController.java
@@ -24,36 +24,44 @@ public class ReviewController {
     public Optional<Review> findReviewById(@PathVariable(value = "id") Integer reviewId) {
         return reviewService.findReviewById(reviewId);
     }
+
     @PostMapping
     public Optional<Review> create(@Validated @RequestBody Review review) {
         log.info("Получен POST запрос  /reviews, {}", review);
         return reviewService.create(review);
     }
+
     @PutMapping
     public Optional<Review> update(@Validated @RequestBody Review review) {
         log.info("Получен PUT запрос  /reviews, {}", review);
         return reviewService.updateReview(review);
     }
+
     @DeleteMapping("/{id}")
     public Optional<Review> delete(@PathVariable(value = "id") Integer reviewId) {
         return reviewService.deleteReview(reviewId);
     }
+
     @PutMapping("/{id}/like/{userId}")
     public Optional<Review> addLike(@PathVariable(value = "id") Integer reviewId, @PathVariable Integer userId) {
         return reviewService.addLike(reviewId, userId);
     }
+
     @PutMapping("/{id}/dislike/{userId}")
     public Optional<Review> addDislike(@PathVariable(value = "id") Integer reviewId, @PathVariable Integer userId) {
         return reviewService.addDislike(reviewId, userId);
     }
+
     @DeleteMapping("/{id}/like/{userId}")
     public Optional<Review> removeLike(@PathVariable(value = "id") Integer reviewId, @PathVariable Integer userId) {
         return reviewService.removeLike(reviewId, userId);
     }
+
     @DeleteMapping("/{id}/dislike/{userId}")
     public Optional<Review> removeDislike(@PathVariable(value = "id") Integer reviewId, @PathVariable Integer userId) {
         return reviewService.addDislike(reviewId, userId);
     }
+
     @GetMapping()
     public List<Review> getReviewByFilmId(@RequestParam(required = false) Integer filmId, @RequestParam(defaultValue = "10", required = false) int count) {
         if (filmId == null) {
